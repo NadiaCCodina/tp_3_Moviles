d6756a518aabcadb06a1eeef5662081287c7410f
NadiaCcod <163307149+NadiaCcod@users.noreply.github.com>
2025-10-02 20:33:22 -0300

    Correccion Salir

--- Tp3_Nadia/app/src/main/java/com/nadia/tp3_nadia/ui/slideshow/SalirViewModel.java
+++ Tp3_Nadia/app/src/main/java/com/nadia/tp3_nadia/ui/slideshow/SalirViewModel.java
@@ -1,16 +1,28 @@
 package com.nadia.tp3_nadia.ui.slideshow;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SalirViewModel extends ViewModel {
+public class SalirViewModel extends AndroidViewModel {
 
-    private final MutableLiveData<String> mText;
+    private MutableLiveData<String> mText;
 
-    public SalirViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is slideshow fragment");
+    public SalirViewModel(@NonNull Application application) {
+        super(application);
+    }
+    public MutableLiveData<String> getmText() {
+
+        if (mText == null) {
+            mText = new MutableLiveData<>();
+
+        }
+        return mText;
+
     }
 
     public LiveData<String> getText() {
